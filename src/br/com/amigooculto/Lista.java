7a9ec7b947e0716ef9ddcee2919ff5171797aa9e
Bruno Lage <brunodila@gmail.com>
2011-05-10 21:54:26 -0300

    Fix the Contacts list

--- src/br/com/amigooculto/Lista.java
+++ src/br/com/amigooculto/Lista.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -21,7 +20,8 @@ public class Lista extends Activity implements OnItemClickListener {
 	private List<String> contatos = null;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.lista);
@@ -32,116 +32,169 @@ public class Lista extends Activity implements OnItemClickListener {
 		lstContatos.setOnItemClickListener(this);
 	}
 
-	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-		
+	public void onItemClick(AdapterView<?> parent, View v, int position, long id) 
+	{	
 		String nomeContato = lstContatos.getItemAtPosition(position).toString(); 
 		String idContato= getIdContato(nomeContato);
 		
 		finish(nomeContato,getTelefoneContato(idContato),getEMail(idContato));
 	}
 	
-	protected void addContatosNaLista(){
+	protected void addContatosNaLista()
+	{
 		contatos = new ArrayList<String>();
 		
 		Cursor cursor = getContentResolver().query(
 				ContactsContract.Contacts.CONTENT_URI,null, null, null, 
-					ContactsContract.Contacts.DISPLAY_NAME); 
-		while (cursor.moveToNext()) {
-		   //String contactId = cursor.getString(cursor.getColumnIndex( 
-		   //ContactsContract.Contacts._ID)); 
-		   
-		   String name = cursor.getString(
-                   cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-		   
-		   String hasPhone = cursor.getString(cursor.getColumnIndex(
-				   ContactsContract.Contacts.HAS_PHONE_NUMBER)); 
-		   
-		   if (hasPhone.equalsIgnoreCase("1")) { 
+					ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+		
+		try
+		{
+			cursor.moveToFirst();
+			
+			while (cursor.moveToNext()) 
+			{
+			   String name = cursor.getString(
+	                   cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+			   
 			   contatos.add(name);
-		   }
+			}
+		}
+		finally
+		{
+			cursor.close();
 		}
 	}
 	
-	public String getIdContato(String nome){
+	public String getIdContato(String nome)
+	{
 		String idContato = null;
 		
 		Cursor cursor = getContentResolver().query(
 				ContactsContract.Contacts.CONTENT_URI,null, 
-				null, null, null); 
-		while (cursor.moveToNext()) {
-		   String contactId = cursor.getString(cursor.getColumnIndex( 
-		   ContactsContract.Contacts._ID)); 
-		   
-		   String name = cursor.getString(
-                   cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-		   
-		   if (name.equals(nome)) {
-			   idContato = contactId;
-		   }
+				null, null, null);
+		
+		try
+		{
+			cursor.moveToFirst();
+			
+			while (cursor.moveToNext()) 
+			{
+			   String contactId = cursor.getString(cursor.getColumnIndex( 
+			   ContactsContract.Contacts._ID)); 
+			   
+			   String name = cursor.getString(
+	                   cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+			   
+			   if (name.equals(nome)) 
+			   {
+				   idContato = contactId;
+			   }
+			}
+			
+			return idContato;
 		}
-		return idContato;
+		finally
+		{
+			cursor.close();
+		}
 	}
 	
-	public String getTelefoneContato(String id) {
-		
+	public String getTelefoneContato(String id) 
+	{	
 		Cursor cursor = getContentResolver().query(
 				ContactsContract.Contacts.CONTENT_URI,null, 
 				ContactsContract.Contacts._ID + " = " + id, null, null); 
-		
-		cursor.moveToNext();
-		
-		String hasPhone = cursor.getString(cursor.getColumnIndex(
-				   ContactsContract.Contacts.HAS_PHONE_NUMBER)); 
-		   if (hasPhone.equalsIgnoreCase("1")) { 
+	
+		try
+		{
+			cursor.moveToFirst();
+			
+			String hasPhone = cursor.getString(cursor.getColumnIndex(
+					   ContactsContract.Contacts.HAS_PHONE_NUMBER));
+			
+		   if (hasPhone.equalsIgnoreCase("1")) 
+		   { 
 		      // You know it has a number so now query it like this
 		      Cursor phones = getContentResolver().query(
 		    		  ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
 		    		  null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ 
 		    		  id, null, null);
 		      
-		      String phoneNumber = "-";
-		      
-		      while (phones.moveToNext()) {
-		         phoneNumber = phones.getString(
-		        		 phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.NUMBER));
-		      } 
-		      phones.close();
-		      
-		      return phoneNumber;
-		   }
-		   else {
-			   return null;
-		   }
+		      try
+		      {
+		    	  phones.moveToFirst();
+		    	  
+			      String phoneNumber = "-";
+			      
+			      while (phones.moveToNext()) {
+			         phoneNumber = phones.getString(
+			        		 phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.NUMBER));
+			      }
+			      
+			      return phoneNumber;
+		      }
+		      finally
+		      {
+			      phones.close();
+		      }
+			}
+			else 
+			{
+				   return null;
+			}
+		}
+		finally
+		{
+			cursor.close();
+		}
 	}
 	
-	public String getEMail(String id) {
+	public String getEMail(String id) 
+	{
 		Cursor cursor = getContentResolver().query(
 				ContactsContract.Contacts.CONTENT_URI,null, 
 				ContactsContract.Contacts._ID + " = " + id, null, null); 
 		
-		cursor.moveToNext();
-		
-		Cursor emails = getContentResolver().query(
-				   ContactsContract.CommonDataKinds.Email.CONTENT_URI, 
-				   null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + 
-				   id, null, null);
-		
-		String emailAddress = null;
-		   while (emails.moveToNext()) {
-		      // This would allow you get several email addresses 
-		      emailAddress = emails.getString( 
-		      emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-		   } 
-		   emails.close();
-		   
-		   return emailAddress;
+		try
+		{	
+			cursor.moveToFirst();
+			
+			Cursor emails = getContentResolver().query(
+					   ContactsContract.CommonDataKinds.Email.CONTENT_URI, 
+					   null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + 
+					   id, null, null);
+			
+			try
+			{
+				String emailAddress = null;
+				
+				emails.moveToFirst();
+				
+				while (emails.moveToNext()) 
+				{
+				      // This would allow you get several email addresses 
+				      emailAddress = emails.getString( 
+				      emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+				}
+				return emailAddress;
+			}
+			finally
+			{
+				emails.close();
+			}  
+		}
+		finally
+		{
+			cursor.close();	
+		}
 	}
 	
 	public void finish(String nome, String telefone, String eMail) {
 		Intent data = new Intent();
-		data.putExtra("returnNome", nome);
-		data.putExtra("returnTelefone", telefone);
-		data.putExtra("returnEMail", eMail);
+		data.putExtra("returnNome", nome == null ? "-" : nome);
+		data.putExtra("returnTelefone", telefone == null ? "0" : telefone);
+		data.putExtra("returnEMail", eMail == null ? "-" : eMail);
 		setResult(RESULT_OK, data);
 		super.finish();
 	}
